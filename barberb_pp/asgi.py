ad20f96011570d9f0d431e1722a319c4c0c58ea2
MikeBoss80 <miguel.bolivar3000@.com>
2025-04-27 07:46:20 -0500

    Estructura

--- barberb_pp/asgi.py
+++ barberb_pp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for barberb project.
+ASGI config for barberb_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barberb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barberb_app.settings')
 
 application = get_asgi_application()
