07d31671069485709bd41b6a71c120138d0f241d
Eliza01 <eliza22758@gmail.com>
2025-04-28 22:43:03 -0500

    ADD Logo & Login
    se agregar img de fongo y logo, se agrega vista de login

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from core.views import HomepageView
+from core.views import HomepageView, LoginView
 
 """//🔥 Nota: Usamos Class-Based View (HomePageView) lo cual es mas moderno."""
 
 
 urlpatterns = [
     path('', HomepageView.as_view(), name='main'),
+    path('login/', LoginView.as_view(), name='login'),
     
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,8 @@ class HomepageView(TemplateView):
     """Vista principal Main//Home"""
     
     template_name = 'core/main.html'
+
+class LoginView(TemplateView):
+    """Vista de inicio de sesión Login"""
+    
+    template_name = 'core/Login.html'
