321dda98435ec591dab9bd61343f562683685ed5
MikeBoss80 <miguel.bolivar3000@.com>
2025-04-29 01:19:04 -0500

    Creacion app admin_module
    Modulo completo de administrador con los templates necesarios a dia de hoy

---
+++ admin_module/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render 
+from django.views.generic import TemplateView, ListView
+
+
+# Create your views here.
+
+class HomeadminView(TemplateView, ListView):
+    """Vista Principal Modulo Admin"""
+    template_name = 'admin_module/main.html'
+    
+
+
+class CitasView(TemplateView):
+    """Vista  citas"""
+    template_name = 'admin_module/citas.html'
+
+
+
+class BarberosView(TemplateView):
+     template_name= 'admin_module/barberos.html'
+     
+     
+class ServiciosView(TemplateView):
+     template_name= 'admin_module/servicios.html'
+
+class ContenidosView(TemplateView):
+     template_name= 'admin_module/contenidos.html'
+
+     
+class InventarioView(TemplateView):
+     template_name= 'admin_module/inventario.html'
+
+
+class ReportesView(TemplateView):
+     template_name= 'admin_module/reportes.html'
+
+
+class SeguridadView(TemplateView):
+     template_name= 'admin_module/seguridad.html'
+     
+
+class SoporteView(TemplateView):
+     template_name= 'admin_module/soporte.html'
+     
--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from core.views import HomepageView
+from .views import HomepageView
 
 """//🔥 Nota: Usamos Class-Based View (HomePageView) lo cual es mas moderno."""
 
 
 urlpatterns = [
     path('', HomepageView.as_view(), name='main'),
-    
 ]
