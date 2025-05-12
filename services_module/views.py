995b428d6d63371177781c9f55afe17ae9ac4db3
JsD1834 <josedanilosantosserrato04@gmail.com>
2025-05-12 03:12:41 -0500

    UPD listado de servicios
    - Se agrega proxy para llamada al API de Maps
    - Se agrega vista del listado de acuerdo los resultados de la busqueda

--- services_module/views.py
+++ services_module/views.py
@@ -3,12 +3,34 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 from django.http import JsonResponse
 from django.conf import settings
+from django.views import View
+from urllib.request import urlopen, Request
+import json
 # Create your views here.
 
 class MapServicesView(TemplateView):
     """Vista Principal Servicios"""
     template_name = 'map_services.html'
 
+class PlacesListView(View):
+    def get(self, request):
+        context = {
+        }
+        return render(request, 'places_list.html', context)
+
 def getMap(request):
-    #Devuelve la clave de la API de Google Maps como respuesta JSON
     return JsonResponse({'mapApiKey': settings.MAPS_APIKEY})
+
+def getPlacesBySearch(request):
+    print(request)
+    query = request.GET.get('query')
+    # lat = request.GET.get('lat')
+    # lng = request.GET.get('lng')
+    loc = request.GET.get('location')
+    url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&location={loc}&key={settings.MAPS_APIKEY}"
+    print(url)
+    req = Request(url)
+    with urlopen(req) as response:
+        datos = response.read()
+        data = json.loads(datos)
+    return JsonResponse(data)
